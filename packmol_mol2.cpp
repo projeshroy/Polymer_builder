29c0759c4e2505af6580ed5420af1a9042fb2ec2
projeshroy <projesh.roy@gmail.com>
2023-02-22 22:47:15 +0530

    2023-02-22_224710

--- packmol_mol2.cpp
+++ packmol_mol2.cpp
@@ -29,9 +29,10 @@ int main(int argc, char** argv){
 	input_file >> read_string >> molecule_count;
 	packed_count.resize(molecule_count);
 	mol2_file_address.resize(molecule_count);
+	input_file >> read_string;
 
 	for(int i = 0; i < molecule_count; i++){//make sure no of molecules and their order in packmol.inp and input.in are the same !
-		input_file >> read_string >> mol2_file_address[i] >> packed_count[i];
+		input_file >> mol2_file_address[i] >> packed_count[i];
 	}
 
 //................................................................................................
@@ -85,9 +86,9 @@ int main(int argc, char** argv){
 	Total_atom_charges.resize(Total_atoms);
 	Total_mol2_bond_types.resize(Total_bonds);
 	Total_mol2_bonds.resize(Total_bonds, 2);
-	int start_atom_index = -Atoms_per_molecule[0];
-	int start_bond_index = -Bonds_per_molecule[0];
-	int start_fragment_index = -fragments_per_molecule[0];
+	int start_atom_index = 0;
+	int start_bond_index = 0;
+	int start_fragment_index = 0;
 
 	for(int i = 0; i < molecule_count; i++){	
 		Vec_s Element_names, atom_type_names, fragment_names, mol2_bond_types;
@@ -102,14 +103,10 @@ int main(int argc, char** argv){
 			  atom_charges, mol2_bonds, mol2_bond_types);
 
 		for(int j = 0; j < packed_count[i]; j++){
-			start_atom_index += Atoms_per_molecule[i];
-			start_bond_index += Bonds_per_molecule[i];
-			start_fragment_index += fragments_per_molecule[i];
 			Mat_i mat_start_atom_index; mat_start_atom_index.resize(mol2_bonds.rows(), mol2_bonds.cols());
 			mat_start_atom_index.setOnes(); mat_start_atom_index *= start_atom_index;
 			Vec_i vec_start_fragment_index; vec_start_fragment_index.resize(fragment_indices.rows());
 			vec_start_fragment_index.setOnes(); vec_start_fragment_index *= start_fragment_index;
-	
 		        Total_Element_names.block(start_atom_index, 0,	Atoms_per_molecule[i], 1) = Element_names;
 			Total_atom_type_names.block(start_atom_index, 0, Atoms_per_molecule[i], 1) = atom_type_names;
 			Total_fragment_names.block(start_atom_index, 0, Atoms_per_molecule[i], 1) = fragment_names;
@@ -117,6 +114,9 @@ int main(int argc, char** argv){
 			Total_fragment_indices.block(start_atom_index, 0, Atoms_per_molecule[i], 1) = fragment_indices+vec_start_fragment_index;
 			Total_mol2_bond_types.block(start_bond_index, 0, Bonds_per_molecule[i], 1) = mol2_bond_types;
 			Total_mol2_bonds.block(start_bond_index, 0, Bonds_per_molecule[i], 2) = mol2_bonds+mat_start_atom_index; 
+			start_atom_index += Atoms_per_molecule[i];
+			start_bond_index += Bonds_per_molecule[i];
+			start_fragment_index += fragments_per_molecule[i];
 		}
 	}
 
